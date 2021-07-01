e4e36416c26b855472bf2ea384f2a2d41ff135d9
Neighborpil <neighborpil@naver.com>
2021-07-01 13:05:18 +0900

    save

---
+++ codes/10/10.04/js/lineChart.js
@@ -0,0 +1,135 @@
+/*
+*    lineChart.js
+*    Mastering Data Visualization with D3.js
+*    10.4 - Converting our code to OOP
+*/
+
+class LineChart {
+	// constructor function - make a new visualization object.
+	constructor(_parentElement, _coin) {
+		this.parentElement = _parentElement;
+		this.coin = _coin;
+
+		this.initVis();
+	}
+
+	// initVis method - set up static parts of our visualization.
+	initVis() {
+		const vis = this;
+
+		vis.MARGIN = { LEFT: 50, RIGHT: 20, TOP: 50, BOTTOM: 50 }
+		vis.WIDTH = 350 - vis.MARGIN.LEFT - vis.MARGIN.RIGHT;
+		vis.HEIGHT = 250 - vis.MARGIN.TOP - vis.MARGIN.BOTTOM;
+
+		vis.svg = d3.select(vis.parentElement).append('svg')
+			.attr('width', vis.WIDTH + vis.MARGIN.LEFT + vis.MARGIN.RIGHT)
+			.attr('height', vis.HEIGHT + vis.MARGIN.TOP + vis.MARGIN.BOTTOM);
+
+		vis.g = vis.svg.append('g')
+			.attr('transform', `translate(${vis.MARGIN.LEFT}, ${vis.MARGIN.TOP})`);
+
+		// time parsers/formatters
+		vis.parseTime = d3.timeParse("%d/%m/%Y");
+		vis.formatTime = d3.timeFormat("%d/%m/%Y");
+		// for tooltip
+		vis.bisectDate = d3.bisector(d => d.date).left;
+
+		// add the line for the first time
+		vis.g.append('path')
+			.attr('class', 'line')
+			.attr('fill', 'none')
+			.attr('stroke', 'grey')
+			.attr('stroke-width', '3px');
+
+		// axis labels
+		vis.xLabel = vis.g.append('text')
+			.attr('class', 'x axisLabel')
+			.attr('y', vis.HEIGHT + 50)
+			.attr('x', vis.WIDTH / 2)
+			.attr('font-size', '20px')
+			.attr('text-anchor', 'middle')
+			.text(vis.coin);
+		vis.yLabel = vis.g.append('text')
+			.attr('class', 'y axisLabel')
+			.attr('transform', 'rotate(-90)')
+			.attr('y', -60)
+			.attr('x', -170)
+			.attr('font-size', '20px')
+			.attr('text-anchor', 'middle')
+			.text('Price ($)');
+
+		// scales
+		vis.x = d3.scaleTime().range([0, vis.WIDTH]);
+		vis.y = d3.scaleLinear().range([vis.HEIGHT, 0]);
+
+		// axis generators
+		vis.xAxisCall = d3.axisBottom()
+			.ticks(3);
+
+		vis.yAxisCall = d3.axisLeft()
+			.ticks(6)
+			.tickFormat(d => `${parseInt(d / 1000)}k`);
+
+		// axis groups
+		vis.xAxis = vis.g.append('g')
+			.attr('class', 'x axis')
+			.attr('transform', `translate(0, ${vis.HEIGHT})`);
+		vis.yAxis = vis.g.append('g')
+			.attr('class', 'y axis');
+
+		vis.wrangleData();
+	}
+
+	// wrangleData method - selecting/filtering the data that we want to use.
+	wrangleData() {
+		const vis = this;
+
+		// filter data based on selections
+		vis.yValue = $("#var-select").val();
+		vis.sliderValues = $('#date-slider').slider('values');
+		vis.dataTimeFiltered = filteredData[vis.coin].filter(d => {
+			return ((d.date >= vis.sliderValues[0]) && (d.date <= vis.sliderValues[1]))
+		});
+
+		vis.updateVis();
+	}
+
+	// updateVis method - updating our elements to match the new data.
+	updateVis() {
+		const vis = this;
+
+		vis.t = d3.transition().duration(1000);
+
+		// update scales
+		vis.x.domain(d3.extent(vis.dataTimeFiltered, d => d.date));
+		vis.y.domain([
+			d3.min(vis.dataTimeFiltered, d => d[vis.yValue]) / 1.005,
+			d3.max(vis.dataTimeFiltered, d => d[vis.yValue]) * 1.005
+		]);
+
+		// fix for format values
+		const formatSi = d3.format('.2s');
+
+		function formatAbbreviation(x) {
+			const s = formatSi(x);
+			switch(s[s.length - 1]) {
+				case 'G': return s.slice(0, -1) + 'B'; // billions
+				case 'K': return s.slice(0, -1) + 'K'; // thousands
+			}
+
+			return s;
+		}
+
+		// update axes
+		vis.xAxisCall.scale(vis.x);
+		vis.xAxis.transition(vis.t).call(vis.xAxisCall);
+		vis.yAxisCall.scale(vis.y);
+		vis.yAxis.transition(vis.t).call(vis.yAxisCall.tickFormat(formatAbbreviation));
+
+		// clear old tooltips
+		vis.g.select('.focus').remove();
+		vis.g.select('.overlay').remove();
+
+		
+	}
+}
